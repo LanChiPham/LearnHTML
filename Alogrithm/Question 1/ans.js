fc304616511b997b6361c8de3f0fd5aa0fc18b78
LanChiPham <phamlanchitit@gmail.com>
2020-09-23 01:01:51 +0800

    Bài tập về Alogrithm 1

---
+++ Alogrithm/Question 1/ans.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var nums = [3,2,4];
+var target = 6;
+
+
+//var twoSum =  function(nums, target) { 
+    function twoSum(nums,target){
+    var result;
+
+
+
+    // code o day 
+
+
+
+
+
+    return result;
+
+};
+
+console.log(twoSum(nums,target));
+
