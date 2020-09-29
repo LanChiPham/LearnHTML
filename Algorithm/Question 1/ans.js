3b4222591c9bea518f8f27153bd66abdf0bc3562
LanChiPham <phamlanchitit@gmail.com>
2020-09-29 16:48:28 +0800

    bai tap 1 update 2

--- Algorithm/Question 1/ans.js
+++ Algorithm/Question 1/ans.js
@@ -3,25 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var nums = [3, 2, 4];
-var target = 6;
-
+var nums = new Array([3, 2, 4]);
+var target = 5;
 
 //var twoSum =  function(nums, target) { 
-const twoSum = function twoSum(nums, target) {
-    var result ;
+var twoSum = function twoSum(nums, target) {
+    var result= nums[i] + nums[j];
 
     for (var i = 0; i < nums.length; i++) {
         for (var j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                
-            }
-        }
-    }
+            if (nums[i] + nums[j] === target) {
+              
+            
+            };
+        };
+    };
   
     return result;
 
-}
+};
+var nums = new Array([2,7,11,15]);
+var target = 9;
 
 console.log(twoSum(nums, target));
+
+
 
